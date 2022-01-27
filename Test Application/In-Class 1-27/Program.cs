07869961df50f97cee39bf751d5219e344e8dcf3
Kylie Merz <kmerz@ou.edu>
2022-01-27 10:50:17 -0600

    Created a simple console application.

---
+++ Test Application/In-Class 1-27/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string firstname = "John";
+Console.WriteLine("What is your last name?");
+string lastname = Console.ReadLine();
+
+Console.WriteLine($"Hello {firstname} {lastname} it's nice to meet you.");
